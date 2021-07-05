c84860c3b86673b87bf6f89cc8d8b5b3800e72bb
rotterandras <rotterandras@gmail.com>
2021-07-05 21:26:31 +0200

    DirectionTest ready

--- src/test/java/com/pp/snake/model/DirectionTest.java
+++ src/test/java/com/pp/snake/model/DirectionTest.java
@@ -1,11 +1,11 @@
 package com.pp.snake.model;
 
-import org.graalvm.compiler.nodes.calc.LeftShiftNode;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.awt.*;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +15,6 @@ class DirectionTest {
     @ParameterizedTest
     @MethodSource("oppositeArguments")
     void getOpposite(Direction input, Direction expected) {
-        Direction direction = input;
         assertEquals(expected, input.getOpposite());
     }
 
@@ -28,7 +27,27 @@ class DirectionTest {
         );
     }
 
-    @Test
-    void getNext() {
+    @ParameterizedTest
+    @MethodSource("getNextArguments")
+    void getNext(Direction direction, Point point, Point expected) {
+        Point result = direction.getNext(point, getThirty, getThirty);
+        assertEquals(expected, result);
     }
+
+    Supplier<Integer> getThirty = () -> 30;
+
+
+    static Stream<Arguments> getNextArguments() {
+        return Stream.of(
+                Arguments.arguments(Direction.LEFT, new Point(31, 30), new Point(30, 30)),
+                Arguments.arguments(Direction.LEFT, new Point(0, 30), new Point(0, 30)),
+                Arguments.arguments(Direction.RIGHT, new Point(10, 10), new Point(11, 10)),
+                Arguments.arguments(Direction.RIGHT, new Point(30, 10), new Point(30, 10)),
+                Arguments.arguments(Direction.UP, new Point(10, 10), new Point(10, 9)),
+                Arguments.arguments(Direction.UP, new Point(10, 0), new Point(10, 0)),
+                Arguments.arguments(Direction.DOWN, new Point(10, 10), new Point(10, 11)),
+                Arguments.arguments(Direction.DOWN, new Point(10, 30), new Point(10, 30))
+        );
+    }
+
 }
