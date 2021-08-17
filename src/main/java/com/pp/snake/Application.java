a7d4c1da10983963e6c861c17929e4073e9c44ae
rotterandras <rotterandras@gmail.com>
2021-08-17 22:08:34 +0200

    minor refactor

--- src/main/java/com/pp/snake/Application.java
+++ src/main/java/com/pp/snake/Application.java
@@ -3,37 +3,37 @@ package com.pp.snake;
 import com.pp.snake.model.Direction;
 import com.pp.snake.presenter.Game;
 import com.pp.snake.view.GamePanel;
-import java.awt.AWTEvent;
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.Toolkit;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
-import javax.swing.JFrame;
 
 public class Application {
 
-  public static void main(String... args) {
-    EventQueue.invokeLater(Application::start);
-  }
+    public static void main(String... args) {
+        EventQueue.invokeLater(Application::start);
+    }
 
-  private static void start() {
-    Game game = new Game();
-    Toolkit.getDefaultToolkit().addAWTEventListener(e -> {
-      KeyEvent ke = KeyEvent.class.cast(e);
-      Direction direction = (ke.getKeyCode() == KeyEvent.VK_LEFT) ? Direction.LEFT
-        : (ke.getKeyCode() == KeyEvent.VK_RIGHT) ? Direction.RIGHT : (ke.getKeyCode() == KeyEvent.VK_UP) ? Direction.UP : (ke.getKeyCode() == KeyEvent.VK_DOWN) ? Direction.DOWN : null;
-      if(direction != null) {
-        game.setNextDirection(direction);
-      }
-    }, AWTEvent.KEY_EVENT_MASK);
-    GamePanel gamePanel = new GamePanel(game::getSnakePoints, game::getFieldHeight, game::getFieldWidth);
-    JFrame frame = new JFrame();
-    frame.getContentPane().setLayout(new BorderLayout());
-    frame.getContentPane().add(gamePanel, BorderLayout.CENTER);
-    frame.pack();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-    game.start(gamePanel::repaint);
-  }
+    private static void start() {
+        Game game = new Game();
+        Toolkit.getDefaultToolkit().addAWTEventListener(e -> {
+            KeyEvent ke = KeyEvent.class.cast(e);
+            Direction direction = (ke.getKeyCode() == KeyEvent.VK_LEFT) ? Direction.LEFT
+                    : (ke.getKeyCode() == KeyEvent.VK_RIGHT) ? Direction.RIGHT
+                    : (ke.getKeyCode() == KeyEvent.VK_UP) ? Direction.UP
+                    : (ke.getKeyCode() == KeyEvent.VK_DOWN) ? Direction.DOWN : null;
+            if (direction != null) {
+                game.setNextDirection(direction);
+            }
+        }, AWTEvent.KEY_EVENT_MASK);
+        GamePanel gamePanel = new GamePanel(game::getSnakePoints, game::getFieldHeight, game::getFieldWidth);
+        JFrame frame = new JFrame();
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(gamePanel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        game.start(gamePanel::repaint);
+    }
 
 }
--- src/main/java/com/pp/snake/presenter/Game.java
+++ src/main/java/com/pp/snake/presenter/Game.java
@@ -4,70 +4,72 @@ import com.pp.snake.model.Direction;
 import com.pp.snake.model.Field;
 import com.pp.snake.model.Snake;
 import com.pp.snake.model.SnakeElement;
-import java.awt.Point;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
-import javax.swing.JOptionPane;
 
 public class Game {
 
-  private final Field field;
-  private final Snake snake;
-  private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-  private Runnable refresh;
-  private Direction direction = Direction.RIGHT;
-  private Direction nextDirection = null;
-  private int frame = 0;
+    private final Field field;
+    private final Snake snake;
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private Runnable refresh;
+    private Direction direction = Direction.RIGHT;
+    private Direction nextDirection = null;
+    private int frame = 0;
 
-  public Game() {
-    field = new Field(40, 20);
-    snake = new Snake(new SnakeElement(new Point(5, 5)), new SnakeElement(new Point(4, 5)), new SnakeElement(new Point(3, 5)));
-  }
-
-  public void start(Runnable refresh) {
-    this.refresh = refresh;
-    executorService.scheduleAtFixedRate(this::tick, 0, 100, TimeUnit.MILLISECONDS);
-  }
-
-  public void setNextDirection(Direction direction) {
-    nextDirection = direction;
-  }
-
-  private void tick() {
-    frame++;
-
-    // "tolatás" esetén a kígyó nem hal meg csak megy tovább
-    if (nextDirection == direction.getOpposite()) {
-      nextDirection = null;
+    public Game() {
+        field = new Field(40, 20);
+        snake = new Snake(new SnakeElement(new Point(5, 5)),
+                new SnakeElement(new Point(4, 5)), new SnakeElement(new Point(3, 5)));
     }
-    if(nextDirection != null) {
-      direction = nextDirection;
-      nextDirection = null;
+
+    public void start(Runnable refresh) {
+        this.refresh = refresh;
+        executorService.scheduleAtFixedRate(this::tick, 0, 100, TimeUnit.MILLISECONDS);
     }
-    Point headPoint = frame % 10 == 0 ? snake.grow(direction, field::getWidth, field::getHeight)
-      : snake.move(direction, field::getWidth, field::getHeight);
-    refresh.run();
-    boolean collide = snake.snakeElements()
-      .skip(1)
-      .anyMatch(element -> element.getPoint().equals(headPoint));
-    if(collide) {
-      executorService.shutdown();
-      JOptionPane.showMessageDialog(null, "Game Over!");
+
+    public void setNextDirection(Direction direction) {
+        nextDirection = direction;
     }
-  }
 
-  public Stream<Point> getSnakePoints() {
-    return snake.snakeElements().map(SnakeElement::getPoint);
-  }
+    private void tick() {
+        frame++;
 
-  public int getFieldHeight() {
-    return field.getHeight();
-  }
+        // "tolatás" esetén a kígyó nem hal meg csak megy tovább
+        if (nextDirection == direction.getOpposite()) {
+            nextDirection = null;
+        }
+        if (nextDirection != null) {
+            direction = nextDirection;
+            nextDirection = null;
+        }
+        Point headPoint = frame % 10 == 0 ? snake.grow(direction, field::getWidth, field::getHeight)
+                : snake.move(direction, field::getWidth, field::getHeight);
+        refresh.run();
+        boolean collide = snake.snakeElements()
+                .skip(1)
+                .anyMatch(element -> element.getPoint().equals(headPoint));
+        if (collide) {
+            executorService.shutdown();
+            JOptionPane.showMessageDialog(null, "Game Over!");
+        }
+    }
 
-  public int getFieldWidth() {
-    return field.getWidth();
-  }
+    public Stream<Point> getSnakePoints() {
+        return snake.snakeElements().map(SnakeElement::getPoint);
+    }
+
+    public int getFieldHeight() {
+        return field.getHeight();
+    }
+
+    public int getFieldWidth() {
+        return field.getWidth();
+    }
 
 }
